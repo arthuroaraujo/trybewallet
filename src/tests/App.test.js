76f5c83232f281e91e8059bc275369af60587784
Arthur <arthuroaraujo@gmail.com>
2022-10-03 19:08:03 -0300

    Req5

---
+++ src/tests/App.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { renderWithRouterAndRedux } from './helpers/renderWith';
+import App from '../App';
+
+describe('Testando a aplicação', () => {
+  test('Testando a tela inicial', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    const { pathname } = history.location;
+    const emailInputEl = screen.getByTestId('email-input');
+    const passwordInputEl = screen.getByTestId('password-input');
+    const buttonEl = screen.getByRole('button', {
+      name: /entrar/i,
+    });
+
+    expect(emailInputEl).toBeInTheDocument();
+    expect(passwordInputEl).toBeInTheDocument();
+    expect(buttonEl).toBeInTheDocument();
+    expect(buttonEl).toBeDisabled();
+
+    userEvent.type(emailInputEl, 'email@email.com');
+    userEvent.type(passwordInputEl, '111111');
+
+    expect(buttonEl).toBeEnabled();
+
+    userEvent.click(buttonEl);
+
+    expect(pathname).toBe('/carteira');
+  });
+
+  test('Testando a tela da carteira', () => {
+    const { history } = renderWithRouterAndRedux(<App />, { initialEntries: ['/carteira'] });
+    const { pathname } = history.location;
+    const emailEl = screen.getByTestId('email-field');
+    const totalEl = screen.getByTestId('total-field');
+    const currEl = screen.getByTestId('header-currency-field');
+    const valueEl = screen.getByTestId('value-input');
+    const descEl = screen.getByTestId('description-input');
+    const buttonEl = screen.getByRole('button', {
+      name: /adicionar despesa/i,
+    });
+
+    expect(emailEl).toBeInTheDocument();
+    expect(totalEl).toBeInTheDocument();
+    expect(currEl).toBeInTheDocument();
+    expect(valueEl).toBeInTheDocument();
+    expect(descEl).toBeInTheDocument();
+    expect(buttonEl).toBeInTheDocument();
+
+    userEvent.click(buttonEl);
+
+    expect(pathname).toBe('/carteira');
+  });
+});
